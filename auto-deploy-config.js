356bcb37c10bba008d13ebca6c9772c72f1f47ad
Todd Anderson <bustardcelly@gmail.com>
2016-02-05 10:47:06 -0500

    adding deployment with shipit for CI integration.

---
+++ auto-deploy-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  default: {
+    workspace: '/tmp/red5pro-server-frontend',
+    dirToCopy: '/tmp/red5pro-server-frontend/dist',
+    deployTo: '/opt/local/red5pro-server/webapps',
+    repositoryUrl: 'git@github.com:red5pro/red5pro-server-frontend.git',
+    ignores: [
+      '.git',
+      '.gitignore',
+      'auto-deploy-config.custom.js',
+      'auto-deploy-config.js',
+      'package.json',
+      'README.md',
+      'shipitfile.js',
+      'node_modules/'
+    ],
+    keepReleases: 2,
+    deleteOnRollback: false,
+    shallowClone: true
+  },
+  staging: {
+    branch: 'qa',
+    servers: [
+      'ec2-54-174-142-30.compute-1.amazonaws.com',
+    ]
+  },
+  feature: {
+    branch: '#target_feature_branch#',
+    servers: [
+      'ec2-54-174-142-30.compute-1.amazonaws.com'
+    ]
+  },
+  production: {
+    branch: 'master',
+    servers: [
+      'ec2-54-174-142-30.compute-1.amazonaws.com'
+    ]
+  }
+}
