a8ee157ae4ccfbd6bfabfca7ce645d3f45e991c8
bustardcelly <bustardcelly@gmail.com>
2016-05-19 21:51:30 -0400

    allow lib dependency directory for remote webapps.

--- scripts/task/deploy.js
+++ scripts/task/deploy.js
@@ -2,13 +2,14 @@
 
 var path = require('path');
 var rm = require('del');
+var flatten = require('gulp-flatten');
 
-module.exports = function(distDir, deployDir, gulp) {
+module.exports = function(distDir, deployWebappDir, deployLibDir, gulp) {
 
   gulp.task('clean-deploy', ['build'], function(cb) {
-    rm(deployDir, function(err) {
+    rm(deployWebappDir, function(err) {
       if(err) {
-        console.error('Error in removing ' + deployDir + ': ' + err);
+        console.error('Error in removing ' + deployWebappDir + ': ' + err);
       }
       cb(err);
     });
@@ -16,14 +17,22 @@ module.exports = function(distDir, deployDir, gulp) {
 
   gulp.task('copy-dist', ['clean-deploy'], function(cb) {
     gulp.src([distDir, 'webapps', '**', '*'].join(path.sep))
-        .pipe(gulp.dest(deployDir))
+        .pipe(gulp.dest(deployWebappDir))
         .on('end', cb);
   });
+
+  gulp.task('copy-libs', ['copy-dist'], function(cb) {
+    gulp.src([distDir, 'webapps', '**', 'lib', '**', '*.jar'].join(path.sep))
+        .pipe(flatten())
+        .pipe(gulp.dest(deployLibDir))
+        .on('end', cb);
+  })
 
   gulp.task('deploy', [
                         'build',
                         'clean-deploy',
-                        'copy-dist'
+                        'copy-dist',
+                        'copy-libs'
                         ]);
 
 };
