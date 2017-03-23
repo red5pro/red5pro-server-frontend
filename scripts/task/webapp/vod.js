ece1cfebc1063c54a68f63dfb956d4bc2505c8f7
bustardcelly <bustardcelly@gmail.com>
2017-03-23 15:17:52 -0400

    getting vod in pipeline of build.

---
+++ scripts/task/webapp/vod.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var gutil = require('gulp-util');
+var WebAppBuilder = require('./WebAppBuilder');
+
+module.exports = function(srcDir, distDir, gulp, templateOptions) {
+
+  var webappDirName = 'vod';
+  var generateTaskLabel = ['generate-webapp', webappDirName].join('-');
+  var copyContentsTaskLabel = ['copy-contents-webapp', webappDirName].join('-');
+  var copyStaticTaskLabel = ['copy-static-webapp', webappDirName].join('-');
+
+  var Builder = new WebAppBuilder(webappDirName, srcDir, distDir,
+                                  gulp, templateOptions);
+
+  return function(initChain) {
+
+    gulp.task(generateTaskLabel, [initChain], function(cb) {
+      gutil.log('Generating Webapps Page: ' + webappDirName);
+      Builder.generateIndexPage(cb);
+    });
+
+    gulp.task(copyContentsTaskLabel, [generateTaskLabel], function(cb) {
+      // Add relative file paths to exclude to the array.
+      // * File paths are relative to the src/webapps/<your-webapp> directory
+      Builder.copyWebappContents([
+        'flash.jsp',
+        'index.jps',
+        'playerProductInstall.swf',
+        'Red5ProServerTests.swf',
+        'SimplePlayer.swf',
+        'streams.jsp',
+        'swfobject.js'
+      ], cb);
+    });
+
+    gulp.task(copyStaticTaskLabel, [copyContentsTaskLabel], function(cb) {
+      // Add relative file paths to exclude to the array.
+      // * File paths are relative to the src/static directory
+      Builder.copyStatic([], cb);
+    });
+
+    /**
+     * Add additional tasks, ensuring the dependency chain as shown by the above tasks.
+     * <pre>
+     *  gulp.task(<taskName>, [<previous-dependency-task-name>], function(cb) { <operation> });
+     * </pre>
+     */
+
+    return [
+      generateTaskLabel,
+      copyContentsTaskLabel,
+      copyStaticTaskLabel
+      /* Add any additional task names with properly dependency chain defined. */
+    ];
+
+  };
+
+};
