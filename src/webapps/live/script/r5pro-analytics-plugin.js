4cea588b73975c8a8fdd7087709250f19db1e75f
bustardcelly <bustardcelly@gmail.com>
2018-07-11 11:10:26 +0200

    some cleanup of analytics.

--- src/webapps/live/script/r5pro-analytics-plugin.js
+++ src/webapps/live/script/r5pro-analytics-plugin.js
@@ -3,24 +3,10 @@
   'use strict';
 
   var isOpen = false;
-  /*
-  var a_protocol = window.analytics_protocol || 'ws';
-  var a_host = window.analytics_host || 'localhost';
-  var a_port = window.analytics_port || 8000;
-  */
-  /*
-  secondscreenClient.setLogLevel(secondscreenClient.LogLevels.INFO);
-  secondscreenClient.log.info(secondscreenClient.versionStr());
-  secondscreenClient.on('state', function(data) {
-    var json = JSON.parse(data);
-    console.log('Message from Host: (' + json.state + ')<br>&nbsp;&nbsp; - ' + json.message);
-  });
-  */
-
   var firstReport;
-  var protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-  var port = protocol === 'wss' ? 8083 : 8081;
-  var host = window.targetHost;
+  var protocol = window.analytics_protocol || (window.location.protocol === 'https:' ? 'wss' : 'ws');
+  var port = window.analytics_port || (protocol === 'wss' ? 8083 : 8081);
+  var host = window.analytics_host || window.targetHost;
   var ws = new WebSocket(protocol + '://' + host + ':' + port + '/live/analyze');
   ws.onmessage = function (msg) {
     console.log('[ws]:: ' + msg);
@@ -39,7 +25,6 @@
     var msg = 'id=' + id + ';action=' + type + ';message=' +  encodeURIComponent(message) + ';';
     if (isOpen) {
       try {
-        // secondscreenClient.send('report', msg);
         var now = new Date().getTime();
         if (!firstReport) {
           firstReport = now;
