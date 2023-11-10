4d82ab5b282b32286038e3cdca4193cb70b48782
Todd Anderson <bustardcelly@gmail.com>
2023-11-10 11:44:10 -0500

    change from red5pro.com to red5.net.

--- src/webapps/live/script/r5pro-analytics-plugin.js
+++ src/webapps/live/script/r5pro-analytics-plugin.js
@@ -1,72 +1,82 @@
 /*
 Copyright © 2015 Infrared5, Inc. All rights reserved.
 
-The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code") 
-is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following  
-license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying  
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code")
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying
 code  constitutes your acceptance of the following license terms and conditions.
 
-Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation 
-files (collectively, the "Software") without restriction, including without limitation the rights to use, 
-copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation
+files (collectively, the "Software") without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
 persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end 
-user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.   
-An  example  of  the EULA can be found on our website at: https://account.red5pro.com/assets/LICENSE.txt.
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.
+An  example  of  the EULA can be found on our website at: https://account.red5.net/assets/LICENSE.txt.
 
 The above copyright notice and this license shall be included in all copies or portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT  
-NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND  
-NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 /* global window, WebSocket */
-(function (window) {
-  'use strict';
+;(function (window) {
+  'use strict'
 
-  var isOpen = false;
-  var firstReport;
-  var protocol = window.analytics_protocol || (window.location.protocol === 'https:' ? 'wss' : 'ws');
-  var port = window.analytics_port || (protocol === 'wss' ? 8083 : 8081);
-  var host = window.analytics_host || window.targetHost;
-  var ws = new WebSocket(protocol + '://' + host + ':' + port + '/live/analyze');
+  var isOpen = false
+  var firstReport
+  var protocol =
+    window.analytics_protocol ||
+    (window.location.protocol === 'https:' ? 'wss' : 'ws')
+  var port = window.analytics_port || (protocol === 'wss' ? 8083 : 8081)
+  var host = window.analytics_host || window.targetHost
+  var ws = new WebSocket(protocol + '://' + host + ':' + port + '/live/analyze')
   ws.onmessage = function (msg) {
-    console.log('[ws]:: ' + msg);
+    console.log('[ws]:: ' + msg)
   }
   ws.onopen = function () {
-    console.log('[red5pro-analytics]:: socket opened.');
-    isOpen = true;
+    console.log('[red5pro-analytics]:: socket opened.')
+    isOpen = true
   }
   ws.onerror = function (event) {
-    console.error('[red5pro-analytics]:: socket error');
+    console.error('[red5pro-analytics]:: socket error')
     console.error(event)
-    isOpen = false;
+    isOpen = false
   }
 
   window.r5pro_ws_send = function (id, type, message) {
-    var msg = 'id=' + id + ';action=' + type + ';message=' +  encodeURIComponent(message) + ';';
+    var msg =
+      'id=' +
+      id +
+      ';action=' +
+      type +
+      ';message=' +
+      encodeURIComponent(message) +
+      ';'
     if (isOpen) {
       try {
-        var now = new Date().getTime();
+        var now = new Date().getTime()
         if (!firstReport) {
-          firstReport = now;
+          firstReport = now
         }
-        ws.send(JSON.stringify({
-          sendBroadcast: id.split('+').join('_'),
-          ts: now,
-          data: {
-            message: msg,
-            elapsed: now - firstReport
-          }
-        }));
+        ws.send(
+          JSON.stringify({
+            sendBroadcast: id.split('+').join('_'),
+            ts: now,
+            data: {
+              message: msg,
+              elapsed: now - firstReport,
+            },
+          })
+        )
       } catch (e) {
-        console.warn(e);
+        console.warn(e)
       }
-      return;
+      return
     }
   }
-
-})(window);
+})(window)
